a6de61bfe0bc32a989a4c13ebb319236563242cb
Alan Oliveira <alancjobm@gmail.com>
2024-04-01 08:10:57 -0300

    feat: add default value in param

--- application/ATM/domain/cash_machine.rb
+++ application/ATM/domain/cash_machine.rb
@@ -1,86 +1,88 @@
 module Application
   module ATM
-    class CashMachine
+    module Domain
+      class CashMachine
 
-      attr_reader :available, :cash_notes
+        attr_reader :available, :cash_notes
 
-      def initialize(id:, available: false, cash_notes: [])
-        @id = id
-        @available = available
-        @cash_notes = cash_notes
-      end
-
-      def add_notes(new_cash_notes)
-        raise StandardError.new("Para abastecer o caixa é necessário adicionar nota") if new_cash_notes.empty?
-
-        if @cash_notes.empty?
-          @cash_notes = new_cash_notes
-
-          return render_response_json(cash_notes: new_cash_notes)
+        def initialize(id:, available: false, cash_notes: [])
+          @id = id
+          @available = available
+          @cash_notes = cash_notes
         end
 
-        return render_response_json(cash_notes: new_cash_notes, errors: ["caixa-em-uso"]) if @available
+        def add_notes(new_cash_notes: [])
+          raise StandardError.new("Para abastecer o caixa é necessário adicionar nota") if new_cash_notes.empty?
 
-        @cash_notes.sort_by!(&:value); new_cash_notes.sort_by!(&:value)
+          if @cash_notes.empty?
+            @cash_notes = new_cash_notes
 
-        new_cash_notes.each_with_index { |new_note, index| @cash_notes[index].increase_quantity(new_note.quantity) }
+            return render_response_json(cash_notes: new_cash_notes)
+          end
 
-        render_response_json(cash_notes: new_cash_notes)
-      end
+          return render_response_json(cash_notes: new_cash_notes, errors: ["caixa-em-uso"]) if @available
 
-      def withdraw(remove_cash_notes)
-        # to-do
-      end
+          @cash_notes.sort_by!(&:value); new_cash_notes.sort_by!(&:value)
 
-      def total_value
-        @cash_notes.map do |note|
-          note.value * note.quantity
-        end.reduce(:+)
-      end
+          new_cash_notes.each_with_index { |new_note, index| @cash_notes[index].increase_quantity(new_note.quantity) }
 
-      def notes_quantity
-        quantity = {
-          10  => 0,
-          20  => 0,
-          50  => 0,
-          100 => 0
-        }
-
-        return quantity if @cash_notes.empty?
-
-        @cash_notes.each do |note|
-          quantity[note.value] += note.quantity
+          render_response_json(cash_notes: new_cash_notes)
         end
 
-        quantity
+        def withdraw(remove_cash_notes)
+          # to-do
+        end
+
+        def total_value
+          @cash_notes.map do |note|
+            note.value * note.quantity
+          end.reduce(:+)
+        end
+
+        def notes_quantity
+          quantity = {
+            10  => 0,
+            20  => 0,
+            50  => 0,
+            100 => 0
+          }
+
+          return quantity if @cash_notes.empty?
+
+          @cash_notes.each do |note|
+            quantity[note.value] += note.quantity
+          end
+
+          quantity
+        end
+
+        def update_availability(availability)
+          @available = availability
+        end
+
+        private
+
+        def update_cash_note()
+          # to-do
+        end
+
+        def build_hash_from_all_notes(cash_notes)
+          notes_result = cash_notes.map { |note| { "#{note.name}" => note.quantity } }
+
+          notes_result.reduce({}, :merge)
+        end
+
+        def render_response_json(cash_notes:, errors: [])
+          {
+            "caixa" => {
+              "caixaDisponivel" => @available,
+              "notas" => build_hash_from_all_notes(cash_notes)
+            },
+            "erros" => errors
+          }
+        end
+
       end
-
-      def update_availability(availability)
-        @available = availability
-      end
-
-      private
-
-      def update_cash_note()
-        # to-do
-      end
-
-      def build_hash_from_all_notes(cash_notes)
-        notes_result = cash_notes.map { |note| { "#{note.name}" => note.quantity } }
-
-        notes_result.reduce({}, :merge)
-      end
-
-      def render_response_json(cash_notes:, errors: [])
-        {
-          "caixa" => {
-            "caixaDisponivel" => @available,
-            "notas" => build_hash_from_all_notes(cash_notes)
-          },
-          "erros" => errors
-        }
-      end
-
     end
   end
 end
--- application/ATM/domain/cash_note.rb
+++ application/ATM/domain/cash_note.rb
@@ -1,40 +1,42 @@
 module Application
   module ATM
-    class CashNote
+    module Domain
+      class CashNote
 
-      attr_reader :id, :value, :quantity
+        attr_reader :id, :value, :quantity
 
-      def initialize(id: nil, value:, quantity: 0)
-        @id = id
-        @value = value
-        @quantity = quantity
+        def initialize(id: nil, value:, quantity: 0)
+          @id = id
+          @value = value
+          @quantity = quantity
+        end
+
+        def name
+          {
+            10  => "notasDez",
+            20  => "notasVinte",
+            50  => "NotasCinquenta",
+            100 => "notasCem"
+          }[value]
+        end
+
+        def increase_quantity(quantity)
+          @quantity += quantity
+        end
+
+        def virtual_note?
+          @id.nil?
+        end
+
+        def to_hash
+          {
+            id: @id,
+            value: @value,
+            quantity: @quantity,
+          }
+        end
+
       end
-
-      def name
-        {
-          10  => "notasDez",
-          20  => "notasVinte",
-          50  => "NotasCinquenta",
-          100 => "notasCem"
-        }[value]
-      end
-
-      def increase_quantity(quantity)
-        @quantity += quantity
-      end
-
-      def virtual_note?
-        @id.nil?
-      end
-
-      def to_hash
-        {
-          id: @id,
-          value: @value,
-          quantity: @quantity,
-        }
-      end
-
     end
   end
 end
